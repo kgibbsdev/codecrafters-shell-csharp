fc92c166c9c7a299df10efe3c08fdfcdadb1f3e7
kgibbsdev <kyletgibbs@gmail.com>
2026-02-28 23:13:15 -0500

    codecrafters submit [skip ci]

--- src/main.cs
+++ src/main.cs
@@ -16,7 +16,8 @@ class Program
             }
             else if (arg.StartsWith("type"))
             {
-                string commandTarget = arg.Substring(5);
+                string commandTarget = arg.Substring(5).Trim();
+                // Console.WriteLine("commandTarget: " + commandTarget);
                 switch (commandTarget)
                 {
                     case "echo":
@@ -25,7 +26,37 @@ class Program
                         Console.WriteLine($"{commandTarget} is a shell builtin");
                         break;
                     default:
-                        Console.WriteLine(commandTarget + ":" + " not found");
+                        // Console.WriteLine(commandTarget + ":" + " not found");
+                        var pathVars = System.Environment.GetEnvironmentVariable("PATH");
+                        var paths = pathVars.Split(":");
+                        bool found = false;
+                        for (int i = 0; i < paths.Length; i++)
+                        {
+                            var currentPath = paths[i];
+                            if (!Directory.Exists(currentPath))
+                            {
+                                continue;
+                            }
+
+                            var filePath = Path.Combine(currentPath, commandTarget);
+                            if (File.Exists(filePath))
+                            {
+                               var mode = File.GetUnixFileMode(filePath);
+                               if ((mode & (UnixFileMode.UserExecute | UnixFileMode.GroupExecute |
+                                            UnixFileMode.OtherExecute)) != 0)
+                               {
+                                   Console.WriteLine(commandTarget + " is " + filePath);
+                                   found = true;
+                                   break;
+                               }
+                            }
+                        }
+
+                        if (!found)
+                        {
+                            Console.WriteLine(commandTarget + ":" + " not found");   
+                        }
+
                         break;
                 }
             }
